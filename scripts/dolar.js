35162b31aa37e24415ab420d2bccd867db525338
Marcos Paulo Carneiro Tavares <46692865+marcoscarneiro2@users.noreply.github.com>
2021-06-11 20:03:02 -0300

    Add files via upload

--- scripts/dolar.js
+++ scripts/dolar.js
@@ -1,13 +1,13 @@
 export default function dolar() {
     const moeda = document.querySelector('#valor')
     const nome = document.querySelector('#nome')
-
-    const a = fetch('https://economia.awesomeapi.com.br/json/all/USD')
+   
+    const a = fetch('https://economia.awesomeapi.com.br/json/all/USDT-BRL')
 
     a.then((r) => {
         return r.json()
     }).then((jason) => {
-        nome.innerText = jason.USD.code + ' atual:'
-        moeda.innerText = jason.USD.ask.slice(0, 4)
+        nome.innerText = jason.USDT.code + 'T atual:'
+        moeda.innerText = jason.USDT.bid.slice(0, 4)
     })
 }
